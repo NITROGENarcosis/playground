ce0f152589b2322a41e1c76e09fc9748e718036a
Lamar <lamarbarnett@barnett-it.com>
2015-12-07 22:39:13 +0100

    updated schema to mariadb syntax

--- flask_tests/flaskr/schema.sql
+++ flask_tests/flaskr/schema.sql
@@ -1,7 +1,10 @@
-drop table if exists entries;
-create table entries (
-	id integer primary key autoincrement,
-	title text not null,
-	text text not null
-);
+CREATE TABLE `entries` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `title` TEXT NOT NULL COLLATE 'latin1_bin',
+    `text` TEXT NOT NULL COLLATE 'latin1_bin',
+    PRIMARY KEY (`id`)
+)
+COLLATE='latin1_bin'
+ENGINE=InnoDB
+;
 
