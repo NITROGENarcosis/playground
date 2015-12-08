49f96172170095de08fe769f5b51c3e355b9c157
Lamar <lamarbarnett@barnett-it.com>
2015-12-08 19:18:16 +0100

    added usermgmt page, added prototypes for adding and remvoing users

--- flask_tests/flaskr_modular/views/admin.py
+++ flask_tests/flaskr_modular/views/admin.py
@@ -45,3 +45,22 @@ def logout():
     session.pop('logged_in', None)
     flash('logged out')
     return redirect(url_for('home.show_entries'))
+
+@admin.route('/usermgmt')
+def user_mgmt():
+    if not session.get('logged_in'):
+        abort(401)
+    cur = g.db.cursor()
+    sql = 'select `id`, `username` from `login`'
+    cur.execute(sql)
+    users =[dict(id=row['id'], username=row['username']) for row in cur.fetchall()]
+    print( users )
+    return render_template('admin/user_mgmt.html', users=users)
+
+@admin.route('/adduser')
+def add_user():
+    return redirect(url_for('home.show_entries'))
+
+@admin.route('/deluser')
+def delete_user():
+    return redirect(url_for('home.show_entries'))
