9aaa62467e0ea54b6be1b7296118fc76ee15a849
Lamar <lamarbarnett@barnett-it.com>
2015-12-08 10:18:41 +0100

    added login table to schema

--- flask_tests/flaskr/schema.sql
+++ flask_tests/flaskr/schema.sql
@@ -8,3 +8,13 @@ COLLATE='latin1_bin'
 ENGINE=InnoDB
 ;
 
+CREATE TABLE `login` (
+    `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` TEXT NOT NULL COLLATE 'latin1_bin',
+    `password` TEXT NOT NULL COLLATE 'latin1_bin',
+    PRIMARY KEY (`id`)
+)
+COLLATE='latin1_bin'
+ENGINE=InnoDB
+;
+
